dNG-git/pas_http_core

commit 549b24963736e1b65c110f8bddfd18c3196cee7d

Enhancement: Added more dynamic creation of request body readers Enhancement: Added a context specific link renderer for the "dNG.pas.data.text" namespace Enhancement: Added support for "multipart/form-data" request bodies Enhancement: Added built-in XSS protection for "dNG.pas.data.xhtml.form.Processor" Enhancement: "dNG.pas.data.text.Link" now support context specific preferred base URLs Enhancement: Added support for context specific logging Enhancement: Minimize JavaScript and CSS files through our builders Enhancement: Added FormTags sanitizer Enhancement: Added FormTags encoder and renderer for e-mails Enhancement: Added "info" element to the XHTML form renderer Enhancement: Added support for "link", "user_author_bar" and "user_publisher_bar" to the XHTML OSet parser Enhancement: Updates jQuery to 2.1.1, RequireJS to 2.1.14 Enhancement: Fine tuned "simple" theme for smaller screen sizes Change: Module blocks are now called module controller instances to emphasize the MVC pattern Change: Removed threading feature from "dNG.pas.data.http.RequestBody" Change: Moved "dNG.pas.data.text.FormProcessor" to its own namespace "dNG.pas.data.text.form" as class "Processor" Change: Virtual config paths now store the "_path_prefix" value automatically if not given Change: Link placeholders are generally marked with two underscore characters like "__id__" Change: Replaced references and method names for RFC 2616 by RFC 7231 Change: Moved "dNG.pas.data.text.tag_parser.BlockMixin" from "pas_tag_parser" to "dNG.pas.data.xhtml.tag_parser.BlockMixin" Fix: "dNG.pas.data.text.Link" should not expect a Request singleton to be present for generating links Fix: Fixed exceptions in rewrite mixins if values are not of the expected format Fix: Fixed various API documentation Trivial: Restructured module header

src/dNG/pas/controller/abstract_http_request.py
@@ -2,10 +2,6 @@
 ##j## BOF
 
 """
-dNG.pas.controller.AbstractHttpRequest
-"""
-"""n// NOTE
-----------------------------------------------------------------------------
 direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
@@ -20,8 +16,7 @@ http://www.direct-netware.de/redirect.py?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasHttpCoreVersion)#
 #echo(__FILEPATH__)#
-----------------------------------------------------------------------------
-NOTE_END //n"""
+"""
 
 # pylint: disable=import-error,invalid-name,no-name-in-module
 
@@ -36,6 +31,8 @@ except ImportError: from urllib import quote, unquote
 from dNG.data.rfc.header import Header
 from dNG.pas.data.settings import Settings
 from dNG.pas.data.http.request_body import RequestBody
+from dNG.pas.data.http.request_body_multipart import RequestBodyMultipart
+from dNG.pas.data.http.request_body_urlencoded import RequestBodyUrlencoded
 from dNG.pas.data.text.input_filter import InputFilter
 from dNG.pas.data.text.l10n import L10n
 from dNG.pas.module.named_loader import NamedLoader
@@ -47,9 +44,9 @@ from .stdout_stream_response import StdoutStreamResponse
 try:
 #
 	from dNG.pas.data.session.http_adapter import HttpAdapter as HttpSessionAdapter
-	from dNG.pas.data.session.implementation import Implementation as SessionImplementation
+	from dNG.pas.data.session.implementation import Implementation as Session
 #
-except ImportError: SessionImplementation = None
+except ImportError: Session = None
 
 class AbstractHttpRequest(AbstractRequest, AbstractHttpMixin):
 #
@@ -111,6 +108,10 @@ Constructor __init__(AbstractHttpRequest)
 		"""
 Request body pointer
 		"""
+		self.body_instance = None
+		"""
+Request body instance
+		"""
 		self.inner_request = None
 		"""
 A inner request is used to support protocols based on other ones (e.g.
@@ -120,53 +121,6 @@ JSON-RPC based on HTTP).
 		self.log_handler = NamedLoader.get_singleton("dNG.pas.data.logging.LogHandler", False)
 
 		self.supported_features['inner_request'] = True
-	#
-
-	def configure_request_body(self, request_body, content_type_expected = None):
-	#
-		"""
-Configures the given RequestBody to be read by the Request implementation.
-
-:param request_body: RequestBody instance
-:param content_type_expected: Expected Content-Type header if any to use the
-                              RequestBody instance.
-
-:return: (object) Configured RequestBody instance
-:since:  v0.1.00
-		"""
-
-		_return = None
-
-		if (isinstance(request_body, RequestBody)):
-		#
-			if (content_type_expected != None):
-			#
-				content_type = InputFilter.filter_control_chars(self.get_header("Content-Type"))
-				if (content_type != None): content_type = content_type.lower().split(";", 1)[0]
-			#
-
-			content_length = InputFilter.filter_int(self.get_header("Content-Length"))
-
-			if (self.body_fp != None
-			    and (content_type_expected == None or (content_type != None and content_type == content_type_expected))
-			    and ((content_length != None and content_length > 0)
-			         or "chunked" in Header.get_field_list_dict(self.get_header("Transfer-Encoding"))
-			        )
-			   ):
-			#
-				if (content_length != None): request_body.set_input_size(content_length)
-				else: request_body.define_input_chunk_encoded(True)
-
-				content_encoding = self.get_header("Content-Encoding")
-				if (content_encoding != None): request_body.define_input_compression(content_encoding)
-
-				request_body.set_input_ptr(self.body_fp)
-				self.body_fp = None
-				_return = request_body
-			#
-		#
-
-		return _return
 	#
 
 	def execute(self):
@@ -212,7 +166,7 @@ Executes the incoming request.
 		#
 		except Exception as handled_exception:
 		#
-			if (self.log_handler != None): self.log_handler.error(handled_exception)
+			if (self.log_handler != None): self.log_handler.error(handled_exception, context = "pas_http_core")
 			response.handle_exception(None, handled_exception)
 		#
 
@@ -230,9 +184,11 @@ Executes the given request and generate content for the given response.
 		requested_module = request.get_module()
 		requested_service = "".join([word.capitalize() for word in request.get_service().split("_")])
 
-		if (NamedLoader.is_defined("dNG.pas.module.blocks.{0}.{1}".format(requested_module, requested_service))):
+		if (self.log_handler != None): self.log_handler.debug("{0!r} has been called for 'dNG.pas.module.controller.{1}.{2}'", self, requested_module, requested_service, context = "pas_http_core")
+
+		if (NamedLoader.is_defined("dNG.pas.module.controller.{0}.{1}".format(requested_module, requested_service))):
 		#
-			instance = NamedLoader.get_instance("dNG.pas.module.blocks.{0}.{1}".format(requested_module, requested_service))
+			instance = NamedLoader.get_instance("dNG.pas.module.controller.{0}.{1}".format(requested_module, requested_service))
 			if (self.log_handler != None): instance.set_log_handler(self.log_handler)
 
 			instance.init(request, response)
@@ -241,9 +197,9 @@ Executes the given request and generate content for the given response.
 		#
 		else:
 		#
-			if (NamedLoader.is_defined("dNG.pas.module.blocks.{0}.Module".format(requested_module))):
+			if (NamedLoader.is_defined("dNG.pas.module.controller.{0}.Module".format(requested_module))):
 			#
-				instance = NamedLoader.get_instance("dNG.pas.module.blocks.{0}.Module".format(requested_module))
+				instance = NamedLoader.get_instance("dNG.pas.module.controller.{0}.Module".format(requested_module))
 				if (self.log_handler != None): instance.set_log_handler(self.log_handler)
 
 				instance.init(request, response)
@@ -264,6 +220,54 @@ Returns the inner request instance.
 		"""
 
 		return self.inner_request
+	#
+
+	def get_request_body(self, request_body_instance = None, content_type_expected = None):
+	#
+		"""
+Returns a configured RequestBody instance to be read by the Request
+implementation.
+
+:param request_body_instance: RequestBody instance to be configured
+:param content_type_expected: Expected Content-Type header if any to use the
+                              RequestBody instance.
+
+:return: (object) Configured RequestBody instance
+:since:  v0.1.00
+		"""
+
+		content_type = InputFilter.filter_control_chars(self.get_header("Content-Type"))
+		if (content_type != None): content_type = content_type.lower().split(";", 1)[0]
+
+		_return = (self._init_request_body(content_type)
+		           if (request_body_instance == None) else
+		           request_body_instance
+		          )
+
+		if (isinstance(_return, RequestBody)):
+		#
+			content_length = InputFilter.filter_int(self.get_header("Content-Length"))
+
+			if (self.body_fp != None
+			    and (content_type_expected == None or (content_type != None and content_type == content_type_expected))
+			    and ((content_length != None and content_length > 0)
+			         or "chunked" in Header.get_field_list_dict(self.get_header("Transfer-Encoding"))
+			        )
+			   ):
+			#
+				if (content_length != None): _return.set_input_size(content_length)
+				else: _return.define_input_chunk_encoded(True)
+
+				content_encoding = self.get_header("Content-Encoding")
+				if (content_encoding != None): _return.define_input_compression(content_encoding)
+
+				_return.set_headers(self.get_headers())
+				_return.set_input_ptr(self.body_fp)
+				self.body_fp = None
+			#
+		#
+
+		return _return
 	#
 
 	def handle_missing_service(self, response):
@@ -298,6 +302,29 @@ logged in and/or its timezone is identified.
 		self.timezone = float(Settings.get("core_timezone", (timezone / 3600)))
 	#
 
+	def _init_request_body(self, content_type):
+	#
+		"""
+Returns the RequestBody instance to be read by the Request implementation
+matching the given content type.
+
+:param content_type: Content type to be handled by the RequestBody instance
+
+:return: (object) RequestBody instance; None if not supported
+:since:  v0.1.00
+		"""
+
+		_return = None
+
+		if (content_type == "application/x-www-form-urlencoded"): _return = RequestBodyUrlencoded()
+		elif (content_type[:10] == "multipart/"): _return = RequestBodyMultipart()
+		elif (self.get_header("Content-Length") != None
+		      or self.get_header("Transfer-Encoding") != None
+		     ): _return = RequestBody()
+
+		return _return
+	#
+
 	def _init_response(self):
 	#
 		"""
@@ -313,9 +340,9 @@ Initializes the matching response instance.
 
 		try:
 		#
-			if (SessionImplementation != None):
+			if (Session != None):
 			#
-				session_class = SessionImplementation.get_class()
+				session_class = Session.get_class()
 				session_class.set_adapter(HttpSessionAdapter)
 
 				session = session_class.load(session_create = False)
@@ -332,7 +359,7 @@ Initializes the matching response instance.
 		#
 		except Exception as handled_exception:
 		#
-			if (self.log_handler != None): self.log_handler.error(handled_exception)
+			if (self.log_handler != None): self.log_handler.error(handled_exception, context = "pas_http_core")
 		#
 
 		if (self.lang == ""): self.lang = self.lang_default
@@ -432,21 +459,49 @@ instance.
 		"""
 
 		if (virtual_config == None): _return = None
+		elif ("path_parameters" in virtual_config and virtual_config['path_parameters']):
+		#
+			_return = NamedLoader.get_instance("dNG.pas.controller.PredefinedHttpRequest")
+
+			encoded_parameters = virtual_pathname[len(virtual_config['_path_prefix']):].split("/")
+
+			parameters = { }
+			dsds = { }
+
+			for encoded_parameter in encoded_parameters:
+			#
+				parameter = encoded_parameter.split(" ", 2)
+
+				if (len(parameter) == 2): parameters[parameter[0]] = parameter[1]
+				elif (len(parameter) == 3 and parameter[0] == "dsd"): dsds[parameter[1]] = parameter[2]
+			#
+
+			if ("m" in virtual_config): _return.set_module(virtual_config['m'])
+			elif ("m" in parameters): _return.set_module(parameters['m'])
+
+			if ("s" in virtual_config): _return.set_service(virtual_config['s'])
+			elif ("s" in parameters): _return.set_service(parameters['s'])
+
+			if ("a" in virtual_config): _return.set_action(virtual_config['a'])
+			elif ("a" in parameters): _return.set_action(parameters['a'])
+
+			for key in dsds: _return.set_dsd(key, dsds[key])
+		#
 		elif ("setup_callback" in virtual_config):
 		#
-			if ("uri" in virtual_config):
+			if ("path" in virtual_config):
 			#
-				uri = (virtual_pathname[len(virtual_config['uri_prefix']):]
-				       if ("uri_prefix" in virtual_config and virtual_pathname.lower().startswith(virtual_config['uri_prefix'])) else
-				       virtual_pathname
-				      )
+				path = (virtual_pathname[len(virtual_config['_path_prefix']):]
+				        if ("_path_prefix" in virtual_config and virtual_pathname.lower().startswith(virtual_config['_path_prefix'])) else
+				        virtual_pathname
+				       )
 
-				self.set_dsd(virtual_config['uri'], uri)
+				self.set_dsd(virtual_config['path'], path)
 			#
 
 			_return = virtual_config['setup_callback'](self, virtual_config)
 		#
-		elif ("m" in virtual_config or "s" in virtual_config or "a" in virtual_config or "uri" in virtual_config):
+		elif ("m" in virtual_config or "s" in virtual_config or "a" in virtual_config or "path" in virtual_config):
 		#
 			_return = NamedLoader.get_instance("dNG.pas.controller.PredefinedHttpRequest")
 
@@ -459,14 +514,14 @@ instance.
 				for key in virtual_config['dsd']: _return.set_dsd(key, virtual_config['dsd'][key])
 			#
 
-			if ("uri" in virtual_config):
+			if ("path" in virtual_config):
 			#
-				uri = (virtual_pathname[len(virtual_config['uri_prefix']):]
-				       if ("uri_prefix" in virtual_config and virtual_pathname.lower().startswith(virtual_config['uri_prefix'])) else
-				       virtual_pathname
-				      )
+				path = (virtual_pathname[len(virtual_config['_path_prefix']):]
+				        if ("_path_prefix" in virtual_config and virtual_pathname.lower().startswith(virtual_config['_path_prefix'])) else
+				        virtual_pathname
+				       )
 
-				_return.set_dsd(virtual_config['uri'], uri)
+				_return.set_dsd(virtual_config['path'], path)
 			#
 		#
 
@@ -509,7 +564,7 @@ Respond the request with the given response.
 		#
 		except Exception as handled_exception:
 		#
-			if (self.log_handler != None): self.log_handler.error(handled_exception)
+			if (self.log_handler != None): self.log_handler.error(handled_exception, context = "pas_http_core")
 		#
 
 		AbstractRequest._respond(self, response)
@@ -525,8 +580,22 @@ Sets the inner request object.
 :since: v0.1.00
 		"""
 
-		if (self.log_handler != None): self.log_handler.debug("#echo(__FILEPATH__)# -Request.set_inner_request(+request)- (#echo(__LINE__)#)")
+		if (self.log_handler != None): self.log_handler.debug("#echo(__FILEPATH__)# -{0!r}.set_inner_request()- (#echo(__LINE__)#)", self, context = "pas_http_core")
 		self.inner_request = request
+	#
+
+	def set_session(self, session):
+	#
+		"""
+Sets the associated session.
+
+:param session: (object) Session instance
+
+:since: v0.1.00
+		"""
+
+		AbstractHttpMixin.set_session(self, session)
+		if (isinstance(self.inner_request, AbstractInnerRequest) and self.inner_request.is_supported("session")): self.inner_request.set_session(session)
 	#
 
 	@staticmethod

src/dNG/pas/controller/http_wsgi1_stream_response.py
@@ -2,10 +2,6 @@
 ##j## BOF
 
 """
-dNG.pas.controller.HttpWsgi1StreamResponse
-"""
-"""n// NOTE
-----------------------------------------------------------------------------
 direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
@@ -20,16 +16,11 @@ http://www.direct-netware.de/redirect.py?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasHttpCoreVersion)#
 #echo(__FILEPATH__)#
-----------------------------------------------------------------------------
-NOTE_END //n"""
-
-# pylint: disable=abstract-method
-# pylint 1.1.0 was unable to detect next = __next__ correctly
-
-from collections import Iterator
+"""
 
 from dNG.pas.data.binary import Binary
 from dNG.pas.data.streamer.http_compressed import HttpCompressed as HttpCompressedStreamer
+from dNG.pas.runtime.iterator import Iterator
 from .abstract_http_stream_response import AbstractHttpStreamResponse
 
 class HttpWsgi1StreamResponse(AbstractHttpStreamResponse, Iterator):
@@ -122,9 +113,7 @@ This iterator is only called for uncompressed data.
 			if (self.streamer != None):
 			#
 				_return = (None if (self.streamer.is_eof()) else self.streamer.read())
-
-				if (_return == False): _return = None
-				elif (_return != None): _return = self._prepare_output_data(_return)
+				if (_return != None): _return = self._prepare_output_data(_return)
 			#
 			elif (self.data != None):
 			#
@@ -141,7 +130,6 @@ This iterator is only called for uncompressed data.
 
 		return _return
 	#
-	next = __next__
 
 	def close(self):
 	#

src/dNG/pas/data/http/streaming.py
@@ -2,10 +2,6 @@
 ##j## BOF
 
 """
-dNG.pas.data.http.Streaming
-"""
-"""n// NOTE
-----------------------------------------------------------------------------
 direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
@@ -20,8 +16,7 @@ http://www.direct-netware.de/redirect.py?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasHttpCoreVersion)#
 #echo(__FILEPATH__)#
-----------------------------------------------------------------------------
-NOTE_END //n"""
+"""
 
 from os import path
 import re
@@ -30,6 +25,7 @@ from dNG.pas.controller.abstract_http_response import AbstractHttpResponse
 from dNG.pas.data.mime_type import MimeType
 from dNG.pas.data.translatable_exception import TranslatableException
 from dNG.pas.data.streamer.abstract import Abstract as AbstractStreamer
+from dNG.pas.data.http.translatable_exception import TranslatableException as TranslatableHttpException
 
 class Streaming(object):
 #
@@ -111,6 +107,7 @@ are met.
 					#
 				#
 
+				if (not is_valid): raise TranslatableHttpException("pas_http_core_400", 400)
 				if (not is_content_length_set): response.set_header("Content-Length", streamer.get_size())
 				response.set_streamer(streamer)
 			#

commit 9180695f27b105ab129281a73e3620a9eab3dcba

Change: Removed explicit dependency to "pas_database" in "dNG.pas.xhtml.tag_parser.RewriteUserXhtmlMixin" Fix: Fixed left-over placeholder "[lang]" which should be "__lang__" Fix: Check for "set_range()" requests to return true for streamer instances. Fixes requests with no Range requests broken by commit "549b249637". Fix: Use "dNG.pas.streamer.HttpWsgi1" to return an empty bytes string on EOF if used as a file-like object Trivial: Use "os.path.join()" for formatting paths if applicable

src/dNG/pas/controller/abstract_http_request.py
@@ -422,7 +422,7 @@ Initialize l10n
 
 		lang = (AbstractHttpRequest.filter_parameter(self.parameters['lang']) if ("lang" in self.parameters) else "")
 
-		if (lang != "" and os.access(path.normpath("{0}/{1}/core.json".format(Settings.get("path_lang"), lang)), os.R_OK)): self.lang = lang
+		if (lang != "" and os.access(path.join(Settings.get("path_lang"), lang, "core.json"), os.R_OK)): self.lang = lang
 		else:
 		#
 			if (self.lang_default == ""): lang_rfc_region = Settings.get("core_lang", "en_US")
@@ -434,8 +434,8 @@ Initialize l10n
 			if (Settings.is_defined("pas_http_site_lang_{0}".format(lang_rfc_region))): lang_rfc_region = Settings.get("pas_http_site_lang_{0}".format(lang_rfc_region))
 			elif (Settings.is_defined("pas_http_site_lang_{0}".format(lang_domain))): lang_domain = Settings.get("pas_http_site_lang_{0}".format(lang_domain))
 
-			if (os.access(path.normpath("{0}/{1}/core.json".format(Settings.get("path_lang"), lang_rfc_region)), os.R_OK)): self.lang_default = lang_rfc_region
-			elif (os.access(path.normpath("{0}/{1}/core.json".format(Settings.get("path_lang"), lang_domain)), os.R_OK)): self.lang_default = lang_domain
+			if (os.access(path.join(Settings.get("path_lang"), lang_rfc_region, "core.json"), os.R_OK)): self.lang_default = lang_rfc_region
+			elif (os.access(path.join(Settings.get("path_lang"), lang_domain, "core.json"), os.R_OK)): self.lang_default = lang_domain
 			else: self.lang_default = Settings.get("core_lang", "en")
 		#
 

src/dNG/pas/controller/http_wsgi1_stream_response.py
@@ -20,13 +20,14 @@ http://www.direct-netware.de/redirect.py?licenses;mpl2
 
 from dNG.pas.data.binary import Binary
 from dNG.pas.data.streamer.http_compressed import HttpCompressed as HttpCompressedStreamer
+from dNG.pas.data.streamer.http_wsgi1 import HttpWsgi1 as HttpWsgi1Streamer
 from dNG.pas.runtime.iterator import Iterator
 from .abstract_http_stream_response import AbstractHttpStreamResponse
 
 class HttpWsgi1StreamResponse(AbstractHttpStreamResponse, Iterator):
 #
 	"""
-This stream response instance will write all data to the underlying WSGI
+This stream response instance will write all data to the underlying WSGI 1.0
 implementation.
 
 :author:     direct Netware Group
@@ -87,6 +88,7 @@ python.org: Return an iterator object.
 			#
 			else: streamer = self.streamer
 
+			streamer = HttpWsgi1Streamer(streamer)
 			_return = self.wsgi_file_wrapper(streamer)
 		#
 
@@ -98,7 +100,7 @@ python.org: Return an iterator object.
 		"""
 python.org: Return the next item from the container.
 
-:return: (str) Response data
+:return: (bytes) Response data
 :since:  v0.1.00
 		"""
 

src/dNG/pas/data/http/streaming.py
@@ -68,7 +68,8 @@ are met.
 				is_content_length_set = False
 				is_valid = False
 
-				if (request.get_header('range') != None):
+				if (request.get_header('range') == None): is_valid = True
+				else:
 				#
 					streamer_size = streamer.get_size()
 					range_start = 0
@@ -93,9 +94,7 @@ are met.
 							range_end = streamer_size - 1
 						#
 
-						if (is_valid and range_start > 0): is_valid = streamer.is_supported("seeking")
-
-						if (is_valid and (range_start > 0 or range_end < streamer_size)):
+						if (is_valid):
 						#
 							response.set_header("HTTP/1.1", "HTTP/1.1 206 Partial Content", True)
 							response.set_header("Content-Length", 1 + (range_end - range_start))

src/dNG/pas/data/streamer/http_wsgi1.py
@@ -21,10 +21,10 @@ http://www.direct-netware.de/redirect.py?licenses;mpl2
 from dNG.pas.data.binary import Binary
 from .abstract_encapsulated import AbstractEncapsulated
 
-class HttpCompressed(AbstractEncapsulated):
+class HttpWsgi1(AbstractEncapsulated):
 #
 	"""
-Compressing streamer based on the given compressor.
+WSGI 1.0 compliant streamer.
 
 :author:     direct Netware Group
 :copyright:  (C) direct Netware Group - All rights reserved
@@ -35,27 +35,6 @@ Compressing streamer based on the given compressor.
              Mozilla Public License, v. 2.0
 	"""
 
-	def __init__(self, streamer, compressor):
-	#
-		"""
-Constructor __init__(HttpCompressed)
-
-:param streamer: Encapsulated streamer instance
-:param compressor: Compression object
-
-:since: v0.1.00
-		"""
-
-		AbstractEncapsulated.__init__(self, streamer)
-
-		self.compressor = compressor
-		"""
-Compression function
-		"""
-
-		self.set_io_chunk_size(1048576)
-	#
-
 	def read(self, _bytes = None):
 	#
 		"""
@@ -64,32 +43,12 @@ Reads from the current streamer session.
 :param _bytes: How many bytes to read from the current position (0 means
                until EOF)
 
-:return: (bytes) Data; None if EOF
+:return: (bytes) Data; Empty byte string if EOF
 :since:  v0.1.00
 		"""
 
 		_return = AbstractEncapsulated.read(self, _bytes)
-		is_data_uncompressed = True
-
-		while (self.compressor != None and is_data_uncompressed):
-		#
-			if (_return == None):
-			#
-				_return = self.compressor.flush()
-
-				is_data_uncompressed = False
-				self.compressor = None
-			#
-			else:
-			#
-				_return = self.compressor.compress(Binary.bytes(_return))
-
-				# Feed compressor object with data until it returns at least one byte
-				is_data_uncompressed = (len(_return) < 1)
-				if (is_data_uncompressed): _return = AbstractEncapsulated.read(self, _bytes)
-			#
-		#
-
+		if (_return == None): _return = Binary.BYTES_TYPE()
 		return _return
 	#
 #

src/dNG/pas/data/xhtml/oset/file_parser.py
@@ -90,8 +90,15 @@ Renders content ready for output from the given OSet template.
 		#
 			if (type(template_name) != str): raise TypeException("Given OSet template name is not valid")
 
-			file_pathname = path.normpath("{0}/{1}/{2}.tsc".format(self.path, self.oset, template_name.replace(".", "/")))
-			if (not os.access(file_pathname, os.R_OK)): file_pathname = path.normpath("{0}/{1}/{2}.tsc".format(self.path, Settings.get("pas_http_theme_oset_default", "xhtml5"), template_name.replace(".", "/")))
+			file_pathname = path.join(self.path, self.oset, "{0}.tsc".format(template_name.replace(".", "/")))
+
+			if (not os.access(file_pathname, os.R_OK)):
+			#
+				file_pathname = path.join(self.path,
+				                          Settings.get("pas_http_theme_oset_default", "xhtml5"),
+				                          "{0}.tsc".format(template_name.replace(".", "/"))
+				                         )
+			#
 
 			template_data = (None if (self.cache_instance == None) else self.cache_instance.get_file(file_pathname))
 

src/dNG/pas/data/xhtml/tag_parser/rewrite_user_xhtml_mixin.py
@@ -25,8 +25,10 @@ from dNG.pas.data.text.date_time import DateTime
 from dNG.pas.data.text.l10n import L10n
 from dNG.pas.data.text.tag_parser.rewrite_mixin import RewriteMixin
 from dNG.pas.data.xhtml.formatting import Formatting
-from dNG.pas.database.nothing_matched_exception import NothingMatchedException
 from dNG.pas.module.named_loader import NamedLoader
+
+try: from dNG.pas.database.nothing_matched_exception import NothingMatchedException
+except ImportError: from dNG.pas.runtime.value_exception import ValueException as NothingMatchedException
 
 class RewriteUserXhtmlMixin(RewriteMixin):
 #

src/dNG/pas/data/xhtml/theme/renderer.py
@@ -148,7 +148,7 @@ Checks if the given theme and subtype is supported.
 
 		if (theme != None):
 		#
-			file_pathname = path.normpath("{0}/{1}/{2}.tsc".format(self.path, theme.replace(".", "/"), ("site" if (subtype == None) else subtype)))
+			file_pathname = path.join(self.path, theme.replace(".", "/"), "{0}.tsc".format("site" if (subtype == None) else subtype))
 			_return = os.access(file_pathname, os.R_OK)
 		#
 
@@ -322,11 +322,11 @@ Renders content ready for output from the given OSet template.
 		theme = self.theme
 		theme_subtype = self.theme_subtype
 
-		file_pathname = path.normpath("{0}/{1}/{2}.tsc".format(self.path, theme, theme_subtype))
+		file_pathname = path.join(self.path, theme, "{0}.tsc".format(theme_subtype))
 
 		if (theme_subtype != "site" and (not os.access(file_pathname, os.R_OK))):
 		#
-			file_pathname = path.normpath("{0}/{1}/site.tsc".format(self.path, theme))
+			file_pathname = path.join(self.path, theme, "site.tsc")
 			theme_subtype = "site"
 		#
 
@@ -398,7 +398,7 @@ Sets the theme to use.
 		if (self.log_handler != None): self.log_handler.debug("#echo(__FILEPATH__)# -{0!r}.set({1})- (#echo(__LINE__)#)", self, theme, context = "pas_http_core")
 
 		theme = theme.replace(".", "/")
-		file_pathname = path.normpath("{0}/{1}/site.tsc".format(self.path, theme))
+		file_pathname = path.join(self.path, theme, "site.tsc")
 
 		"""
 Retry with default theme
@@ -408,7 +408,7 @@ Retry with default theme
 		else:
 		#
 			self.theme = Settings.get("pas_http_theme_default", "simple").replace(".", "/")
-			file_pathname = path.normpath("{0}/{1}/site.tsc".format(self.path, self.theme))
+			file_pathname = path.normpath(self.path, self.theme, "site.tsc")
 		#
 
 		"""

src/dNG/pas/module/controller/output/menu/file.py
@@ -72,13 +72,18 @@ Returns a list of rendered links for the service menu.
 		#
 			links = None
 
-			if ("[lang]" in file_pathname):
+			if ("__lang__" in file_pathname):
 			#
-				expanded_file_pathname = file_pathname.replace("[lang]", self.request.get_lang())
-				if (not os.access(path.normpath("{0}/{1}".format(Settings.get("path_base"), expanded_file_pathname)), os.R_OK)): expanded_file_pathname = file_pathname.replace("[lang]", Settings.get("core_lang"))
+				expanded_file_pathname = file_pathname.replace("__lang__", self.request.get_lang())
+
+				if (not os.access(path.join(Settings.get("path_base"), expanded_file_pathname), os.R_OK)):
+				#
+					expanded_file_pathname = file_pathname.replace("__lang__", Settings.get("core_lang"))
+				#
+
 				file_pathname = expanded_file_pathname
 			#
-			else: file_pathname = path.normpath("{0}/{1}".format(Settings.get("path_base"), file_pathname))
+			else: file_pathname = path.join(Settings.get("path_base"), file_pathname)
 
 			json_data = CachedJsonFile.read(file_pathname)
 			if (type(json_data) == list): links = json_data