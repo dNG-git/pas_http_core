a9d56a17a58c8e70a0a79028a4b634eec9abda73
NotTheEvilOne <git-NTEO@vplace.de>
2016-09-13 12:05:19 +0200

    Enhancement: Added possibility to hide the page navigation box Enhancement: Added possibility to change the page navigation title Enhancement: Added setter for "max_pages"

--- src/dNG/data/xhtml/page_links_renderer.py
+++ src/dNG/data/xhtml/page_links_renderer.py
@@ -20,9 +20,11 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 
 from math import floor
 
+from dNG.data.binary import Binary
 from dNG.data.text.l10n import L10n
 from dNG.data.xhtml.link import Link
 from dNG.data.xml_parser import XmlParser
+from dNG.runtime.type_exception import TypeException
 
 class PageLinksRenderer(Link):
 #
@@ -57,9 +59,9 @@ DSD key used for the page value
 		"""
 Content used as replacement for a range of page link hidden
 		"""
-		self.hide_navigation_title = False
+		self.hide_navigation_box = False
 		"""
-True to hide the last and next link
+True to hide the surrounding navigation box
 		"""
 		self.max_pages = 11
 		"""
@@ -77,6 +79,10 @@ Pages available
 		"""
 Parameters used to render each page link
 		"""
+		self.navigation_title = L10n.get("pas_http_core_pages")
+		"""
+Title used as prefix for the page links
+		"""
 		self.type = (_type if (_type is not None) else Link.TYPE_RELATIVE_URL)
 		"""
 Page link type
@@ -92,30 +98,32 @@ Renders the page link navigation bar.
 :since:  v0.2.00
 		"""
 
+		max_pages = (self.pages if (self.max_pages < 1) else self.max_pages)
+
 		rendered_prefix = ""
 		rendered_suffix = ""
 
-		if (self.pages < (self.max_pages + 2)):
+		if (self.pages < (max_pages + 2)):
 		#
 			first_page = 1
 			last_page = self.pages
 		#
-		elif (self.page > -1 and self.page < (self.max_pages - 2)):
+		elif (self.page > -1 and self.page < (max_pages - 2)):
 		#
 			rendered_suffix = "<li>{0}</li>\n<li>{1}</li>".format(self.hidden_pages_content,
 			                                                      self._render_page_link(self.pages)
 			                                                     )
 
 			first_page = 1
-			last_page = self.max_pages
+			last_page = max_pages
 		#
-		elif (self.page < 0 or ((self.page + self.max_pages) - 3) > self.pages):
+		elif (self.page < 0 or ((self.page + max_pages) - 3) > self.pages):
 		#
 			rendered_prefix = "<li>{0}</li>\n<li>{1}</li>\n".format(self._render_page_link(1),
 			                                                        self.hidden_pages_content
 			                                                       )
 
-			first_page = ((self.pages - self.max_pages) + 3)
+			first_page = ((self.pages - max_pages) + 3)
 			last_page = self.pages
 		#
 		else:
@@ -128,8 +136,8 @@ Renders the page link navigation bar.
 			                                                      self._render_page_link(self.pages)
 			                                                     )
 
-			first_page = (self.pages - (self.pages - self.page) - (floor(self.max_pages / 2)) + 2)
-			last_page = (first_page + (self.max_pages - 4))
+			first_page = (self.pages - (self.pages - self.page) - (floor(max_pages / 2)) + 2)
+			last_page = (first_page + (max_pages - 4))
 		#
 
 		rendered_links = ""
@@ -145,17 +153,17 @@ Renders the page link navigation bar.
 			                  )
 		#
 
-		return ("<nav class='pagecontent_box pagecontent_page_navigation'>" +
-		        ("<ul>{0}{1}{2}</ul>".format(rendered_prefix, rendered_links, rendered_suffix)
-		         if (self.hide_navigation_title) else
-		         "<b>{0}</b>: <ul>{1}{2}{3}</ul>".format(L10n.get("pas_http_core_pages"),
-		                                                 rendered_prefix,
-		                                                 rendered_links,
-		                                                 rendered_suffix
-		                                                )
-		        ) +
-		        "</nav>"
-		       )
+		_return = (""
+		           if (self.hide_navigation_box) else
+		           "<nav class='pagecontent_box pagecontent_pages_navigation_box pagecontent_pages_navigation_list'>"
+		          )
+
+		if (self.navigation_title != ""): _return += "<b>{0}</b>: ".format(self.navigation_title)
+		_return += "<ul>{0}{1}{2}</ul>".format(rendered_prefix, rendered_links, rendered_suffix)
+
+		if (not self.hide_navigation_box): _return += "</nav>"
+
+		return _return
 	#
 
 	def _render_page_link(self, page):
@@ -191,6 +199,48 @@ Sets the DSD key used for the page value.
 		"""
 
 		self.dsd_page_key = key
+	#
+
+	def set_navigation_box_visibility(self, visibility):
+	#
+		"""
+Sets the visibility of the surrounding navigation box.
+
+:param visibility: True to render the surrounding navigation box
+
+:since: v0.2.00
+		"""
+
+		self.hide_navigation_box = (not visibility)
+	#
+
+	def set_navigation_title(self, title):
+	#
+		"""
+Sets the navigation title to be used for rendering.
+
+:param title: Navigation title to be used for rendering
+
+:since: v0.2.00
+		"""
+
+		title = Binary.str(title)
+		if (type(title) is not str): raise TypeException("Navigation title given is invalid")
+
+		self.navigation_title = title
+	#
+
+	def set_max_pages(self, pages):
+	#
+		"""
+Sets the maximum number of pages shown non-collapsed.
+
+:param pages: Maximum number of pages shown non-collapsed
+
+:since: v0.2.00
+		"""
+
+		self.max_pages = pages
 	#
 #
 
